e628bc40608f06aa2239d14e0405b6ae3f86fde4
unknown <maho.nakata@gmail.com>
2014-02-12 07:04:15 +0000

    update

--- referencelapack/go.sh
+++ referencelapack/go.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 LAPACKVERSION=3.5.0
 rm -rf lapack-$LAPACKVERSION #lapack-$LAPACKVERSION.tgz 
-#wget http://www.netlib.org/lapack/lapack-$LAPACKVERSION.tgz
+wget http://www.netlib.org/lapack/lapack-$LAPACKVERSION.tgz
 tar xvfz lapack-$LAPACKVERSION.tgz
 cd lapack-$LAPACKVERSION
 patch -p0 < ../patch.for_lapack-$LAPACKVERSION
-cp INSTALL/make.inc.gfortran make.inc
+cp make.inc.example make.inc
 
 echo Building reference BLAS
 cd BLAS/SRC ; make ; cd ../../
@@ -16,10 +16,10 @@ cd lapacke
 make
 echo All done
 cd ..
-cp lapack-$LAPACKVERSION/liblapack.a  c:/Mingw/msys/1.0/local/lib
-cp lapack-$LAPACKVERSION/liblapacke.a c:/Mingw/msys/1.0/local/lib
-cp lapack-$LAPACKVERSION/liblblas.a c:/Mingw/msys/1.0/local/lib/
-cp lapack-$LAPACKVERSION/lapacke/include/lapacke.h c:/Mingw/msys/1.0/local/include/
-cp lapack-$LAPACKVERSION/lapacke/include/lapacke_config.h c:/Mingw/msys/1.0/local/include/
-cp lapack-$LAPACKVERSION/lapacke/include/lapacke_mangling.h c:/Mingw/msys/1.0/local/include/
-cp lapack-$LAPACKVERSION/lapacke/include/lapacke_utils.h c:/Mingw/msys/1.0/local/include/
+cp liblapack.a  c:/Mingw/msys/1.0/local/lib
+cp liblapacke.a c:/Mingw/msys/1.0/local/lib
+cp libblas.a c:/Mingw/msys/1.0/local/lib/
+cp lapacke/include/lapacke.h c:/Mingw/msys/1.0/local/include/
+cp lapacke/include/lapacke_config.h c:/Mingw/msys/1.0/local/include/
+cp lapacke/include/lapacke_mangling.h c:/Mingw/msys/1.0/local/include/
+cp lapacke/include/lapacke_utils.h c:/Mingw/msys/1.0/local/include/
