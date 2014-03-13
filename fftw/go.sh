3d10821714b9e1c33fd2cbad7e2accbb8dc74335
U-maho-forexdevel\maho <maho.nakata@gmail.com>
2014-03-13 19:56:20 +0900

    enable shared

--- fftw/go.sh
+++ fftw/go.sh
@@ -5,6 +5,8 @@ rm -rf fftw-$FFTWVERSION *~ #* fftw-$FFTWVERSION.tar.gz
 wget http://www.fftw.org/fftw-$FFTWVERSION.tar.gz
 tar xvfz fftw-$FFTWVERSION.tar.gz 
 cd fftw-$FFTWVERSION
-./configure --with-our-malloc16 --with-windows-f77-mangling --enable-shared --disable-static --enable-threads --with-combined-threads --enable-sse2
+./configure --with-our-malloc16 --with-windows-f77-mangling --enable-shared --enable-threads --with-combined-threads --enable-sse2
 make
 make install
+cp /usr/local/lib/libfftw3.dll.a /usr/local/lib/libfftw3-3.dll
+
