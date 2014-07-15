f73b0a29b0cd6a301b5e96fdb5b2cf9c164a4ea5
U-maho-forexdevel\maho <maho.nakata@gmail.com>
2014-07-15 20:37:18 +0900

    update to 3.3.4 and consider boundary for SSE

--- fftw/go.sh
+++ fftw/go.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 uname -a
-FFTWVERSION=3.3.3
+FFTWVERSION=3.3.4
 rm -rf fftw-$FFTWVERSION *~ #* fftw-$FFTWVERSION.tar.gz
 wget http://www.fftw.org/fftw-$FFTWVERSION.tar.gz
 tar xvfz fftw-$FFTWVERSION.tar.gz 
 cd fftw-$FFTWVERSION
-./configure --with-our-malloc16 --with-windows-f77-mangling --enable-shared --enable-static --enable-threads --with-combined-threads --enable-sse2
+./configure --with-our-malloc16 --with-windows-f77-mangling --enable-shared --enable-static --enable-threads --with-combined-threads --enable-sse2 -with-incoming-stack-boundary=2
 make
 make install
