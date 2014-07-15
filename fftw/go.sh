13f20ed57bf3acace7e4b74041529f7c626d840b
U-maho-forexdevel\maho <maho.nakata@gmail.com>
2014-07-15 21:08:11 +0900

    enable avx as well

--- fftw/go.sh
+++ fftw/go.sh
@@ -5,6 +5,6 @@ rm -rf fftw-$FFTWVERSION *~ #* fftw-$FFTWVERSION.tar.gz
 wget http://www.fftw.org/fftw-$FFTWVERSION.tar.gz
 tar xvfz fftw-$FFTWVERSION.tar.gz 
 cd fftw-$FFTWVERSION
-./configure --with-our-malloc16 --with-windows-f77-mangling --enable-shared --enable-static --enable-threads --with-combined-threads --enable-sse2 -with-incoming-stack-boundary=2
+./configure --with-our-malloc16 --with-windows-f77-mangling --enable-shared --enable-static --enable-threads --with-combined-threads --enable-sse2 --enable-avx --with-incoming-stack-boundary=2
 make
 make install
